c0fcf6c2830d3cfab1c17db2de7387d8cc42ed66
Kyle McKenna <mckennakyler@gmail.com>
2020-01-30 15:33:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   
   # method genre_count, returns a hash: keys are genre_count => value that is a count of the number of songs in the genre
   def self.genre_count
-    genre_count = {} # new hash
+    genre_count = Hash.new(0) # new hash
     genre_count.each {|genre| genre_count[genre] += 1} # value key set 
     genre_count
   end 
